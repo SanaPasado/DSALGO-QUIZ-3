e5f2d2d11c768c21db1f5b355aebe1f03ab51172
Clyde Cunanan <cscunanan@student.hau.edu.ph>
2026-01-21 08:07:50 +0800

    backend

--- backend/products/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import re_path
-from .views import ProductListAPIView, ProductDetailAPIView
-
-# Accept optional trailing slash so frontend requests with or without
-# a trailing slash will succeed (e.g. /api/products and /api/products/)
-urlpatterns = [
-    re_path(r'^$', ProductListAPIView.as_view(), name='product-list'),
-    re_path(r'^(?P<pk>\d+)/?$', ProductDetailAPIView.as_view(), name='product-detail'),
-]
